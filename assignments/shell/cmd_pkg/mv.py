c8b59c38b106bfda19939a7b1354f7246cc423b3
vnareddy0724 <vahini.5678@gmail.com>
2017-02-14 12:01:06 -0600

    Update mv.py

--- assignments/shell/cmd_pkg/mv.py
+++ assignments/shell/cmd_pkg/mv.py
@@ -1,1 +1,14 @@
 
+
+"""
+    Name: mv
+    Description:
+        It moves the source file and pastes in destination file.
+    Params: 
+        input_file(String) - Path of source file that to be moved.
+        output_file (String) - Path of destination file where the source file gets pasted.
+    Returns: None
+""" 
+
+def mv(input_file,output_file):
+		shutil.move(input_file,output_file)
