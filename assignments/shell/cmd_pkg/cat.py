52e9d34060bdd200509ef01e26c4899ea2fb84bc
vnareddy0724 <vahini.5678@gmail.com>
2017-02-14 11:59:27 -0600

    Update cat.py

--- assignments/shell/cmd_pkg/cat.py
+++ assignments/shell/cmd_pkg/cat.py
@@ -1,1 +1,14 @@
 
+"""
+    Name: cat
+    Description:
+        Dumps a file
+    Params: 
+        file (string) - The file to be dumped
+    Returns: None
+""" 
+
+def cat(file):
+		f=open(file,'r')
+		print(f.read())
+		f.close()
