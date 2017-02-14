ee466926467b8769c6f84ca930d02fde4633c610
vnareddy0724 <vahini.5678@gmail.com>
2017-02-14 12:10:08 -0600

    Update history.py

--- assignments/shell/cmd_pkg/history.py
+++ assignments/shell/cmd_pkg/history.py
@@ -1,1 +1,20 @@
 
+
+"""
+    Name: history
+    Description:
+        It stores the history of all commands executed.
+    Params: 
+        *cmd:List of input commands is an argument.
+    Returns: None
+""" 
+
+def history(*cmd):
+		f=open("history.txt",'r')
+		count=len(f.readlines())
+		count=count+1
+		f=open("history.txt",'a')
+		f.write("\n")
+		f.write(str(count))
+		for x in cmd:
+			f.write("\t"+x)
