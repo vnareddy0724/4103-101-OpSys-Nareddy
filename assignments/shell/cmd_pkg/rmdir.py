dccd513bf92031f418fa037b9c2a77657a0eb534
vnareddy0724 <vahini.5678@gmail.com>
2017-02-14 12:04:47 -0600

    Update rmdir.py

--- assignments/shell/cmd_pkg/rmdir.py
+++ assignments/shell/cmd_pkg/rmdir.py
@@ -1,1 +1,14 @@
 
+"""
+    Name: rmdir
+    Description:
+        Removes the  directory which is passed as an argument.
+    Params: 
+        dir(string) - Path of directory to be removed that is given as argument.
+    Returns: None
+"""
+
+
+def rmdir(dir):
+		shutil.rmtree(dir)
+		print("Removed directory")
