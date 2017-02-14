af4f936e29be7915ff4ff3df3ebda7191d554189
vnareddy0724 <vahini.5678@gmail.com>
2017-02-14 12:02:18 -0600

    Update rm.py

--- assignments/shell/cmd_pkg/rm.py
+++ assignments/shell/cmd_pkg/rm.py
@@ -1,1 +1,15 @@
 
+
+"""
+    Name: rm
+    Description:
+        Removes the file in the directory which is passed as an argument.
+    Params: 
+        file(string) - Path of file to be removed that is given as argument.
+    Returns: None
+""" 
+	
+def rm(file):
+		os.remove(file)
+		print("file removed successfully")
+		
