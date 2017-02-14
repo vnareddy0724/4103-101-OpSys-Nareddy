6402b1f8e3f909f45687f00555e59a0d40a3da34
vnareddy0724 <vahini.5678@gmail.com>
2017-02-14 12:02:58 -0600

    Update wc.py

--- assignments/shell/cmd_pkg/wc.py
+++ assignments/shell/cmd_pkg/wc.py
@@ -1,1 +1,19 @@
 
+
+"""
+    Name: wc
+    Description:
+        Gives the total number of words in a file that is given as input.
+    Params: 
+        file(string) - Path of file for which number of words to be counted.
+    Returns: None
+"""
+	
+def wc(file):
+		f=open(file,'r')
+		wordcount=0
+		for lines in f:
+			f1=lines.split()
+			wordcount=wordcount+len(f1)
+		f.close()
+		print ('Number of words : ', str(wordcount))
