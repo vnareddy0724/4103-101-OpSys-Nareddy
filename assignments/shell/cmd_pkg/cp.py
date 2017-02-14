4af122e603cfe6be0f16d0188ea25ddc3356c140
vnareddy0724 <vahini.5678@gmail.com>
2017-02-14 12:00:20 -0600

    Update cp.py

--- assignments/shell/cmd_pkg/cp.py
+++ assignments/shell/cmd_pkg/cp.py
@@ -1,1 +1,13 @@
 
+"""
+    Name: cp
+    Description:
+        Copies  source file to destination file.
+    Params: 
+        input_file(String) - Path of source file.
+        output_file(String) - Path of destination file.
+    Returns: None
+""" 
+
+def cp(input_file,output_file):
+		shutil.copy(input_file,output_file)
