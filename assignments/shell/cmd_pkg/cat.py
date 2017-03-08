bc4989a728b11ec23c41071a577823fc16786491
vnareddy0724 <vahini.5678@gmail.com>
2017-03-07 21:55:54 -0600

    Update cat.py

--- assignments/shell/cmd_pkg/cat.py
+++ assignments/shell/cmd_pkg/cat.py
@@ -1,14 +1,39 @@
-
 """
     Name: cat
     Description:
         Dumps a file
     Params: 
         file (string) - The file to be dumped
+		inputfile : redirects output to inputfile if > exists
+		count : append output to inputfile if >> exists
+		pipe:checks whether piping exists
     Returns: None
 """ 
+import shutil
 
-def cat(file):
+def cat(file,inputfile,count,pipe):
+	try:
 		f=open(file,'r')
-		print(f.read())
-		f.close()
+		
+		if pipe == 1:
+			f2=open("pipe.txt",'w')
+			for lines in f:
+				f2.write(lines+"\n")	
+		
+		elif inputfile == None:
+			
+			print(f.read())
+			f.close()
+		
+			
+			
+		else:
+			if count == 0:
+				shutil.copy(file,inputfile)
+			else:
+				f1=open(inputfile,'a')
+				for lines in f:
+					f1.write(lines+"\n")
+					
+	except Exception as e:
+		print("\n"+str(e))					
